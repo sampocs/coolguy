051b50c58233b5401eaed8fb3e59170f9657f460
Sam Pochyly <sampocs@Sams-MacBook-Pro-5.local>
2016-07-05 17:58:36 -0500

    pipeline

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
 
+
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
