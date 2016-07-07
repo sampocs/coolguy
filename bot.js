6671864fc0ca089d969d624499cc95c0c5de2fef
Sam Pochyly <sampocs@Sams-MBP-5.hsd1.il.comcast.net>
2016-07-07 15:03:52 -0500

    testing

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "BIG MEEEEEEP";
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
